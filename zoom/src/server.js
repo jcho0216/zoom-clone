448c9cfac9247aad6c9989f4ae5daab284393b9a
Jun Joy <chojuneseo1@gmail.com>
2021-08-01 22:35:57 +0900

    CREATE: websocket server

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -1,10 +1,18 @@
+import http from 'http';
+import WebSocket from "ws"
 import express from "express";
 
 const app = express();
-
-app.listen(3000);
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
+
+const handleListen = () => console.log('Listening on http://localhost:3000');
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, handleListen);
