bdf1f3123ef304bc96a88464f235ad214ef51931
Jun Joy <chojuneseo1@gmail.com>
2021-08-02 20:16:56 +0900

    UPDATE: WebSocket Connection

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -15,4 +15,15 @@ const server = http.createServer(app);
 
 const wss = new WebSocket.Server({ server });
 
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser");
+    socket.on("close", () => {
+        console.log("Disconnected from the Browser")
+    })
+    socket.on("message", (message) => {
+        console.log(message);
+    })
+    socket.send("hello!!!");
+})
+
 server.listen(3000, handleListen);
